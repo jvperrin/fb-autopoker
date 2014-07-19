22ff1d83172f28441a94fc21a77412c5163554c9
Jason Perrin <jasonvperrin@gmail.com>
2014-07-18 22:53:09 -0600

    Start work on a daily limit for pokes

--- fb-autopoker.js
+++ fb-autopoker.js
@@ -1,11 +1,26 @@
 window.onload = load();
 
 function load() {
+  date = new Date();
+  formatted_date = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate();
   chrome.storage.sync.get({
-    interval: 15,
+    interval: 15, // Defaults
     random: false,
-    range: 1
+    range: 1,
+    dayLimit: 300
   }, function(items) {
+
+    var pokes = 0;
+
+    chrome.storage.local.get({
+      dailyPokes: 0,
+      day: formatted_date
+    }, function(local_items) {
+      pokes = local_items.dailyPokes;
+      console.log("Daily pokes so far: " + pokes);
+    });
+
+    console.log(items);
 
     if (items.random) {
 
@@ -13,29 +28,59 @@ function load() {
         var rand = randomIntFromInterval(items.interval - items.range, items.interval + items.range);
 
         setTimeout(function() {
-          poke();
-          loop();
+          chrome.storage.local.get({
+            day: formatted_date
+          }, function(local_items) {
+            pokes = poke(pokes, local_items.day, items.dayLimit);
+            loop();
+          });
         }, rand * 1000);
       }());
 
     } else {
 
       setInterval(function() {
-        poke();
+        chrome.storage.local.get({
+          day: formatted_date
+        }, function(local_items) {
+          pokes = poke(pokes, local_items.day, items.dayLimit);
+        });
       }, items.interval * 1000);
 
     }
   });
 }
 
-function poke() {
+function poke(dailyPokes, day, pokeLimit) {
   console.log("Attempting a poke at " + new Date());
   var poke_links = $('a._42ft._4jy0._4jy3._4jy1.selected[role="button"][rel="async-post"]:contains("Poke Back")');
   if (poke_links.length) {
     poke_links.each(function(index, link) {
-      link.click();
+      date = new Date();
+      formatted_date = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate();
+
+      if (day == formatted_date) {
+        dailyPokes += 1;
+
+        chrome.storage.local.set({
+          dailyPokes: dailyPokes
+        });
+      } else {
+        chrome.storage.local.set({
+          day: formatted_date,
+          dailyPokes: 0
+        });
+      }
+
+      if (dailyPokes < pokeLimit) {
+        console.log("Poke successful! " + dailyPokes);
+        link.click();
+      } else {
+        console.log("Daily limit reached! " + dailyPokes);
+      }
     });
-    console.log("Poke successful!");
+
+    return dailyPokes;
   }
 }
 
--- options.js
+++ options.js
@@ -3,12 +3,16 @@ function save_options() {
   var interval = parseInt(document.getElementById('interval').value, 10);
   var range = parseInt(document.getElementById('range').value, 10);
   var random = document.getElementById('random').checked;
+  var dayLimit = parseInt(document.getElementById('day-limit').value, 10);
 
   if (isNaN(interval)) {
     interval = 15
   }
   if (isNaN(range)) {
     range = 1;
+  }
+  if (isNaN(dayLimit)) {
+    dayLimit = 300
   }
 
   if (range >= interval) {
@@ -18,11 +22,13 @@ function save_options() {
   chrome.storage.sync.set({
     interval: interval,
     random: random,
-    range: range
+    range: range,
+    dayLimit: dayLimit
   }, function() {
     // Update status to let user know options were saved.
     document.getElementById('interval').value = interval;
     document.getElementById('range').value = range;
+    document.getElementById('day-limit').value = dayLimit;
 
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
@@ -39,11 +45,13 @@ function restore_options() {
   chrome.storage.sync.get({
     interval: 15,
     random: false,
-    range: 1
+    range: 1,
+    dayLimit: 300
   }, function(items) {
     document.getElementById('interval').value = items.interval;
     document.getElementById('random').checked = items.random;
     document.getElementById('range').value = items.range;
+    document.getElementById('day-limit').value = items.dayLimit;
   });
 }
 
